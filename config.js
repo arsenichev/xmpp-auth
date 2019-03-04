d71943dff9193dd9f164381940df860f67a8dad4
Sergey <sergey@arsenichev.ru>
2019-03-04 16:43:21 +0300

    improve configuration flexibility
    Added multiple host configuration, params modification function, log level and anonymous hosts options.

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  authServiceUrl: 'https://learn.javascript.ru/auth/xmpp',
-  logPath: '/var/log/ejabberd/xmpp-auth.log'
+    authServiceUrl: {
+      'localhost': 'https://requestbin.jumio.com/yo7tqryo'
+    },
+    authParamsModificationFunction: function (params) {
+        var md5 = require('md5');
+        return {
+            login: params.user + '@' + params.server,
+            password: md5(params.password)
+        };
+    },
+    anonymousHosts: ['anon.localhost'],
+    logPath: 'logs/xmpp-auth.log',
+    logLevel: 'trace' // trace is dangerous (plaintext passwords in logs)
 };
--- index.js
+++ index.js
@@ -9,97 +9,124 @@ var config = require('./config');
 var request = require('request');
 
 var log = bunyan.createLogger({
-  name:    "auth",
-  streams: [{
-    level: 'info', // trace is dangerous (plaintext passwords in logs)
-    path:  config.logPath
-  }]
+    name:    "auth",
+    streams: [{
+        level: config.logLevel,
+        path:  config.logPath
+    }]
 
 });
 
 process.stdin.on('readable', function read() {
-  log.trace("readable");
-  var offset = 0;
-  var buf = process.stdin.read();
-  if (!buf) return;
+    log.trace("readable");
+    var offset = 0;
+    var buf = process.stdin.read();
+    if (!buf) return;
 
-  log.trace("buffer", buf, buf.toString());
+    log.trace("buffer", buf, buf.toString());
 
-  while (true) {
-    // offset is the current message start
-    if (buf.length < offset + 2) {
-      process.stdin.unshift(buf.slice(offset));
-      return;
+    while (true) {
+        // offset is the current message start
+        if (buf.length < offset + 2) {
+            process.stdin.unshift(buf.slice(offset));
+            return;
+        }
+
+        var messageLength = buf.readUInt16BE(offset);
+
+        log.trace("length", messageLength);
+
+        if (offset + 2 + messageLength < buf.length) {
+            process.stdin.unshift(buf.slice(offset));
+            return;
+        }
+
+        offset += 2;
+        // message example: "auth:iliakan:x.javascript.ru:password"
+        var message = buf.slice(offset, messageLength + offset);
+        offset += messageLength;
+
+        message = message.toString();
+        log.trace("message", message);
+
+        message = message.split(':');
+
+        /*
+         Messages:
+         auth:User:Server:Password (check if a username/password pair is correct)
+         isuser:User:Server (check if it’s a valid user)
+         setpass:User:Server:Password (set user’s password)
+         tryregister:User:Server:Password (try to register an account)
+         removeuser:User:Server (remove this account)
+         removeuser3:User:Server:Password (remove this account if the password is correct)
+         */
+        messageQueue.push({
+            command:  message[0],
+            user:     message[1],
+            server:   message[2],
+            password: message[3] // may be undefined
+        });
+
     }
-
-    var messageLength = buf.readUInt16BE(offset);
-
-    log.trace("length", messageLength);
-
-    if (offset + 2 + messageLength < buf.length) {
-      process.stdin.unshift(buf.slice(offset));
-      return;
-    }
-
-    offset += 2;
-    // message example: "auth:iliakan:x.javascript.ru:password"
-    var message = buf.slice(offset, messageLength + offset);
-    offset += messageLength;
-
-    message = message.toString();
-    log.trace("message", message);
-
-    message = message.split(':');
-
-    /*
-     Messages:
-     auth:User:Server:Password (check if a username/password pair is correct)
-     isuser:User:Server (check if it’s a valid user)
-     setpass:User:Server:Password (set user’s password)
-     tryregister:User:Server:Password (try to register an account)
-     removeuser:User:Server (remove this account)
-     removeuser3:User:Server:Password (remove this account if the password is correct)
-     */
-    messageQueue.push({
-      command:  message[0],
-      user:     message[1],
-      server:   message[2],
-      password: message[3] // may be undefined
-    });
-
-  }
 
 });
 
 
 var messageQueue = async.queue(function(message, callback) {
-  log.info("send to service", message.command, message.user);
-  request.post({
-    url:  config.authServiceUrl,
-    form: message
-  }, function(err, httpResponse, body) {
-    if (err) throw(err);
+    if (message.command !== 'auth') {
+        log.info("not supported command", message.command);
+        // success
+        respond(true);
+        callback();
+    } else {
+        log.info("send to service", message.command, message.user);
 
-    log.info("receive from service", body);
+        var data = message;
 
-    switch(body) {
-    case "1":
-      respond(true);
-      callback();
-      break;
-    case "0":
-      respond(false);
-      callback();
-      break;
-    default:
-      log.error("Invalid response (must be 1 or 0)", body, httpResponse);
-      throw new Error("Invalid response");
+        if (typeof config.authParamsModificationFunction === 'function') {
+            data = config.authParamsModificationFunction(message);
+        }
+
+        log.trace("sending data", data);
+
+        if (config.anonymousHosts.indexOf(message.server) !== -1) {
+            respond(true);
+            callback();
+        } else {
+            if (typeof config.authServiceUrl[message.server] === 'undefined') {
+                throw new Error("Undefined authServiceUrl for host" + message.server);
+            } else {
+                request.post({
+                    url:  config.authServiceUrl[message.server],
+                    form: data
+                }, function(err, httpResponse, body) {
+                    if (err) throw(err);
+
+                    log.info("receive from service", body);
+
+                    switch(body) {
+                        case "1":
+                            respond(true);
+                            callback();
+                            break;
+                        case "0":
+                            respond(false);
+                            callback();
+                            break;
+                        default:
+                            log.error("Invalid response (must be 1 or 0)", body);
+                            throw new Error("Invalid response");
+                    }
+                });
+            }
+        }
     }
-  });
+
 }, 1);
 
 function respond(ok) {
-  process.stdout.write(new Buffer([0, 2, 0, ok ? 1 : 0]));
+    log.info("respond to service", ok);
+    process.stdout.write(new Buffer([0, 2, 0, ok ? 1 : 0]));
 }
 
 
@@ -107,21 +134,21 @@ function respond(ok) {
 // make sure log is flushed!
 // @see https://github.com/trentm/node-bunyan/issues/37#issue-6439571
 process.on('uncaughtException', function flush(err) {
-  // prevent infinite recursion
-  process.removeListener('uncaughtException', flush);
+    // prevent infinite recursion
+    process.removeListener('uncaughtException', flush);
 
-  // bonus: log the exception
-  log.error(err);
+    // bonus: log the exception
+    log.error(err);
 
-  if (typeof(log.streams[0]) !== 'object') return;
+    if (typeof(log.streams[0]) !== 'object') return;
 
-  // throw the original exception once stream is closed
-  log.streams[0].stream.on('close', function(streamErr, stream) {
-    throw err;
-  });
+    // throw the original exception once stream is closed
+    log.streams[0].stream.on('close', function(streamErr, stream) {
+        throw err;
+    });
 
-  // close stream, flush buffer to disk
-  log.streams[0].stream.end();
+    // close stream, flush buffer to disk
+    log.streams[0].stream.end();
 });
 
 
